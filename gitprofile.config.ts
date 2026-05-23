bfc23fe54e7391cb09d4a5b90a215421b3f44a71
BENfrost-20 <leopelorosso@gmail.com>
2026-05-23 12:33:57 +0200

    Revise manual project list and update project info
    Updated project list and modified project details.

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -25,7 +25,7 @@ const CONFIG = {
       },
       manual: {
         // Properties for manually specifying projects
-        projects: ['arifszn/gitprofile', 'arifszn/pandora'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
+        projects: [''], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
       },
     },
     external: {
@@ -33,20 +33,11 @@ const CONFIG = {
       // To hide the `External Projects` section, keep it empty.
       projects: [
         {
-          title: 'Project Name',
+          title: '3D-Printed Dyson Vacuum Replacement Part',
           description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-          imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
-        },
-        {
-          title: 'Project Name',
-          description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-          imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
+            'I reverse-engineered and 3D-designed a replacement brush end cap for a Dyson vacuum cleaner. This specific component breaks easily, and Dyson does not sell the spare part individually.',
+          imageUrl: './public/dysonv10endcap.jpg',
+          link: 'https://www.thingiverse.com/thing:7357903',
         },
       ],
     },
